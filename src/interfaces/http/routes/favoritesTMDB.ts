15493838c869b0fee8532ef0614b763a91d22a80
jclagoria <lagoria79@gmail.com>
2024-04-13 18:42:26 -0300

    changes how to manage the calls

--- src/interfaces/http/routes/favoritesTMDB.ts
+++ src/interfaces/http/routes/favoritesTMDB.ts
@@ -8,53 +8,38 @@ import {ListTMDB} from '../../../domain/models/ListTMDB'
 export const favoritesTMDB = Router()
 
 favoritesTMDB.get('/:accountId/favorites/movies', validateAccountID, validatePageNumber, async (req: Request, res: Response, next: NextFunction)=> {
-    try {
-        const accountId = req.params.accountId
-        const languageValue = req.query.language
-        const numberPage = req.query.page
-        const sortByValue = req.query.sort_by
-
-        const paramKey = {
-            action: 'favorite-movies',
-            language: languageValue,
-            pageNumber: numberPage,
-            sort: sortByValue
-        }
-
-        const keyCache = generateKey({url:`/${accountId}/favorites/movies`, params:  paramKey})
-        const urlParam = `/account/${accountId}/favorite/movies?language=${languageValue}&page=${numberPage}&sort_by=${sortByValue}`
-        const favoritesMovies = await fetchData<FavoriteMedia>(keyCache, urlParam)
-
-        res.status(200).json(favoritesMovies)
-    } catch (error) {
-        next(error)
-    }
+    await fetchFavorites('movies', req, res, next)
 })
 
 favoritesTMDB.get('/:accountId/favorites/tv', validateAccountID, validatePageNumber, async (req: Request, res: Response, next:NextFunction)=> {
+    await fetchFavorites('tv', req, res, next)
+})
+
+async function fetchFavorites(type: 'movies' | 'tv', req: Request, res: Response, next: NextFunction) {
     try {
         const accountId = req.params.accountId
-        const numberPage = req.query.page
-        const languageValue = req.query.language
-        const sortByValue = req.query.sort_by
+        const languageValue = req.query.language || 'en-US'
+        const numberPage = req.query.page || 1
+        const sortByValue = req.query.sort_by || 'created_at.asc'
 
-        const urlParam = `/account/${accountId}/favorite/tv?language=${languageValue}&page=${numberPage}&sort_by=${sortByValue}`
         const paramKey = {
-            action: 'favorite-tv',
+            action: `favorite-${type}`,
             language: languageValue,
             pageNumber: numberPage,
             sort: sortByValue
         }
 
-        const keyCache = generateKey({url: `/account/${accountId}/favorite/tv`, params: paramKey})
+        const baseUrl = `/account/${accountId}/favorite/${type}`
+        const keyCache = generateKey({url: `${baseUrl}`, params: paramKey})
+        const urlParam = `${baseUrl}?language=${languageValue}&page=${numberPage}&sort_by=${sortByValue}`
 
-        const responseFavoriteTv = await fetchData(keyCache, urlParam)
-
-        res.status(200).json(responseFavoriteTv)
+        const favoritesResponse = await fetchData(keyCache, urlParam)
+        res.status(200).json(favoritesResponse)
     } catch (error) {
         next(error)
     }
-})
+}
+
 
 favoritesTMDB.get('/:accountId/lists', validateAccountID, validatePageNumber, async (req: Request, res: Response, next: NextFunction)=> {
     try {
--- src/interfaces/http/routes/watchlistMediaTmdb.ts
+++ src/interfaces/http/routes/watchlistMediaTmdb.ts
@@ -9,48 +9,37 @@ export const watchlistMediaTmdb = Router()
 
 watchlistMediaTmdb.get('/:accountId/watchlist/movies', validateAccountID, validatePageNumber,
     async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const accountId = req.params.accountId
-        const pageNumber = req.query.page
-        const language = req.query.language
-        const sortBy = req.query.sort_by
 
-        const urlParam = `/account/${accountId}/watchlist/movies?language=${language}&page=${pageNumber}&sort_by=${sortBy}`
-        const paramKey = {
-            action: 'watchlist-movies',
-            pageNumber: pageNumber,
-            sortBy: sortBy
-        }
-
-        const keyCache = generateKey({url: `/account/${accountId}/watchlist/movies`, params: paramKey})
-        const responseWatchlistMovie = await fetchData<WatchListMovieTMDB>(keyCache, urlParam)
-
-        res.status(200).json(responseWatchlistMovie)
-    } catch (error) {
-        next(error)
-    }
+    await fetchWatchlistMediaType('movies', req, res, next)
 })
 
-watchlistMediaTmdb.get('/:accountId/watchlist/tv', validateAccountID, validatePageNumber, async (req: Request, res: Response, next: NextFunction) => {
+watchlistMediaTmdb.get('/:accountId/watchlist/tv', validateAccountID, validatePageNumber,
+    async (req: Request, res: Response, next: NextFunction) => {
+
+    await fetchWatchlistMediaType('tv', req, res, next)
+})
+
+async function fetchWatchlistMediaType(type: 'movies' | 'tv', req: Request, res: Response, next: NextFunction) {
     try {
         const accountId = req.params.accountId
         const pageNumber = req.query.page
         const languageValue = req.query.language
         const sortByValue = req.query.sort_by
 
-        const urlParam = `/account/${accountId}/watchlist/tv?language=${languageValue}&page=${pageNumber}&sort_by=${sortByValue}`
         const paramKey = {
-            action: 'watchlist-tv',
+            action: `favorite-${type}`,
             language: languageValue,
             pageNumber: pageNumber,
             sortBy: sortByValue
         }
 
-        const keyCache = generateKey({url: `/account/${accountId}/watchlist/tv`, params: paramKey})
-        const responseWatchlistTv = await fetchData<WatchlistTvTMDB>(keyCache, urlParam)
+        const baseUrl = `/account/${accountId}/watchlist/${type}`
+        const keyCache = generateKey({url: baseUrl, params: paramKey})
+        const urlParam = `${baseUrl}?language=${languageValue}$page=${pageNumber}$sort_by=${sortByValue}`
 
-        res.status(200).json(responseWatchlistTv)
+        const watchlistResponse = await fetchData(keyCache, urlParam)
+        res.status(200).json(watchlistResponse)
     } catch (error) {
         next(error)
     }
-})
+}
